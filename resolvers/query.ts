475bcbe8bfff92ac709a5050dbeb9e77d2d9c7d2
AlexRazor <alexrazor1337@gmail.com>
2023-08-09 22:40:07 +0300

    User auth

--- resolvers/query.ts
+++ resolvers/query.ts
@@ -1,4 +1,17 @@
+import { User } from "@prisma/client";
+import getUserId from "../util/get-user-id";
+
 export const Query = {
+    profile(parent, args, { db, req }, info): User {
+        console.log();
+        const userId = getUserId(req);
+        
+        return db.user.findUnique({
+            where: {
+                id: userId
+            }
+        });
+    },
     users(parent, { query }, { db }, info) {
         if (!query) {
             return db.user.findMany();
